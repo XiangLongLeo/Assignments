c2c582031137a80c5fbcffbf1e5bf1a97811f2f6
KevinKaiwenZheng <Zhengkw.Kevin2020@gdhfi.com>
2022-09-27 10:25:43 +0800

    Updated starter_code.js
    deleted the original answers

--- Sept26/starter_code.js
+++ Sept26/starter_code.js
@@ -7,56 +7,47 @@ let inputString = '';
 let currentLine = 0;
 
 process.stdin.on('data', inputStdin => {
-  inputString += inputStdin;
+	inputString += inputStdin;
 });
 
 process.stdin.on('end', _ => {
-  inputString = inputString.trim().split('\n').map(string => {
-    return string.trim();
-  });
+	inputString = inputString.trim().split('\n').map(string => {
+		return string.trim();
+	});
 
-  main();
+	main();
 });
 
 function readline() {
-  return inputString[currentLine++];
+	return inputString[currentLine++];
 }
 
 // Make a Snippet for the code above this and then write your logic in main();
 
 function main() {
-  // Declare and read the number of test cases.
-  var T;
-  T = parseInt(readline());
+	// Declare and read the number of test cases.
+	var T;
+	T = parseInt(readline());
 
-  // Loop over the number of test cases.
-  for (var test_no = 1; test_no <= T; test_no++) {
-    process.stdout.write('Case #' + test_no + ': ');
-    solve();
-  }
+	// Loop over the number of test cases.
+	for (var test_no = 1; test_no <= T; test_no++) {
+		process.stdout.write('Case #' + test_no + ': ');
+		solve();
+	}
 }
 
 function solve() {
-  // Declare variables N and M.
-  var N, M;
-  // Read the integers from the standard input.
-  [N, M] = readline().split(' ').map(x => parseInt(x));
+	// Declare variables N and M.
+	var N, M;
+	// Read the integers from the standard input.
+	[N, M] = readline().split(' ').map(x => parseInt(x));
 
-  // Declare array `C`.
-  var C;
-  // Read integers from the standard input and save them in the array `C`.
-  C = readline().split(' ').map(x => parseInt(x));
+	// Declare array `C`.
+	var C;
+	// Read integers from the standard input and save them in the array `C`.
+	C = readline().split(' ').map(x => parseInt(x));
+	
+	//compute here
 
-  // Declare a variable for sum and set it to 0.
-  var sum = 0;
-  // Loop through vector `C` and sum its values.
-  for (var i = 0; i < N; i++) {
-    sum += C[i];
-  }
-
-  // Compute the value of the sum modulo M.
-  var modulo = sum % M;
-
-  // Print the result onto the standard output.
-  process.stdout.write(modulo + '\n');
+	process.stdout.write(/*put results here */);
 }
