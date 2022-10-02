0a41f119af1847a295a147828820b8cf9072964a
KevinKaiwenZheng <Zhengkw.Kevin2020@gdhfi.com>
2022-10-02 22:10:55 +0800

    UC Qualification Assessment Task 1

---
+++ UC Qualification Assessment/Code/Prime Numbers.js
@@ -0,0 +1,17 @@
+/**
+ * Assignment: Write a program that computes and outputs all prime numbers within 1000
+ * Part of code has been written, complete the program
+ * 
+ * Submission Requirement
+ * filename: Name_Task1 e.g. ThrawnK_Task1.js
+ */
+
+function main() {
+    //fill this array with the computed prime numbers
+    var primes = [];
+
+
+    //output the calculated numbers, do not modify
+    for (let i = 0; i < primes.length; i++)
+        console.log(primes[i]);
+}
